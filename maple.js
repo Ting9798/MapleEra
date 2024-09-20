bbf3aef75983034e8f3bcbb42032f5eef99fb7d7
Ting9798 <h821026888@gmail.com>
2024-09-20 15:49:17 +0800

    react

--- maple.js
+++ maple.js
@@ -22,10 +22,3 @@ document.querySelectorAll(".home-scroll_section").forEach((section) => {
         });
     });
 });
-
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // 切換菜單顯示
-});
